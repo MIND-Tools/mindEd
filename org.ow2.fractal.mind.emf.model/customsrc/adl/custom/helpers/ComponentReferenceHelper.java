61ad87cf7f489c082455525b202ae2a620452b4a
Raphael Proust <raphael.proust@logica.com>
2010-02-19 12:41:35 +0000

    Body implementation
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/IDE/trunk@85 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

---
+++ org.ow2.fractal.mind.emf.model/customsrc/adl/custom/helpers/ComponentReferenceHelper.java
@@ -0,0 +1,59 @@
+package adl.custom.helpers;
+
+import org.eclipse.emf.common.util.URI;
+import org.ow2.fractal.mind.ide.ModelToProjectUtil;
+
+import adl.ArchitectureDefinition;
+import adl.ComponentReference;
+import adl.InterfaceDefinition;
+import adl.custom.util.AdlMergeUtil;
+
+/** <b>Class</b> <i>ComponentReferenceHelper</i><p>
+ * A helper class to associate with component references. Provides specific methods for component references objects.
+ * 
+ * @author proustr
+ * @model kind="Helper"
+ * @extends HelperAdapter
+ */
+public class ComponentReferenceHelper extends HelperAdapter<ComponentReference> {
+	/**
+	 * AdlMergeUtil mergeUtil : Instantiation of a MergeUtil used to recover imports
+	 * 
+	 */
+	private AdlMergeUtil mergeUtil = AdlMergeUtil.getInstance();
+
+	public ComponentReferenceHelper(ComponentReference t) {
+		super(t);
+	}
+
+	/**
+	 * <b>Method</b> <i>getNameFQN</i>
+	 * <p>
+	 * This method returns the fully qualified name of the current reference.
+	 * 
+	 * @return Fully qualified name of reference associated to this helper.
+	 * 
+	 * @author proustr
+	 */
+	public String getNameFQN() {
+		URI tmpName = null;
+		tmpName = ModelToProjectUtil.INSTANCE.resolveAdl(getObject().getReferenceName(), mergeUtil
+				.recoverImports(getObject()));
+		String tmpString = ModelToProjectUtil.INSTANCE.getFQNFromURI(tmpName);
+		if (tmpName == null) return getObject().getReferenceName();
+		else
+			return tmpString;
+	}
+
+	@Override
+	public InterfaceDefinition getInterfaceByName(String interfaceName,
+			String interfaceParentName) {
+		return null;
+	}
+
+	@Override
+	public ArchitectureDefinition getComponentByName(String componentName) {
+		return null;
+	}
+
+}
---
+++ org.ow2.fractal.mind.emf.model/customsrc/adl/custom/helpers/DefaultHelper.java
@@ -0,0 +1,10 @@
+package adl.custom.helpers;
+
+import org.eclipse.emf.ecore.EObject;
+
+public class DefaultHelper extends HelperAdapter<EObject> {
+
+	public DefaultHelper(EObject t) {
+		super(t);
+	}
+}
