8966890fd859ba3215f671cd654920d2a0f3b6b4
Stephane Chomat <stephane.chomat@imag.fr>
2010-03-22 08:24:32 +0000

    cleanup
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/IDE/trunk@189 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- org.ow2.fractal.mind.ide.ui/src/org/ow2/fractal/mind/ide/ui/actions/MindOpenFileAction.java
+++ org.ow2.fractal.mind.ide.ui/src/org/ow2/fractal/mind/ide/ui/actions/MindOpenFileAction.java
@@ -4,17 +4,12 @@ import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.jface.util.OpenStrategy;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.OpenFileAction;
-import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.internal.ide.DialogUtil;
 import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
 import org.eclipse.ui.internal.ide.IIDEHelpContextIds;
-import org.eclipse.ui.part.FileEditorInput;
 import org.ow2.fractal.mind.ide.ui.Activator;
 
 public class MindOpenFileAction extends OpenFileAction {
