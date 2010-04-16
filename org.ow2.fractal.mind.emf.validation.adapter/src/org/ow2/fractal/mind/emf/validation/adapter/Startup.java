e8053325f458e5540d08e25714db7b3237464be1
Raphael Proust <raphael.proust@logica.com>
2010-04-16 15:25:19 +0000

    Intégration du validateur EMF (validation des relations au niveau du modèle)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/IDE/trunk@483 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

---
+++ org.ow2.fractal.mind.emf.validation.adapter/src/org/ow2/fractal/mind/emf/validation/adapter/Startup.java
@@ -0,0 +1,49 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id$
+ */
+
+package org.ow2.fractal.mind.emf.validation.adapter;
+
+import org.eclipse.emf.ecore.EValidator;
+import org.eclipse.ui.IStartup;
+import adl.AdlPackage;
+
+
+
+/**
+ * Installs an EMF <code>EValidator</code> on the Library package when we
+ * start up.  This validator adapts EMF's <code>EValidator</code> API to the
+ * EMF Model Validation Service API.
+ */
+public class Startup
+	implements IStartup {
+
+	/**
+	 * Initializes me.
+	 */
+	public Startup() {
+		super();
+	}
+
+	/**
+	 * Install the validator.
+	 */
+	public void earlyStartup() {
+		EValidator.Registry.INSTANCE.put(
+			AdlPackage.eINSTANCE,
+			new EValidatorAdapter());
+	}
+}
