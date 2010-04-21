60f22082ed6a2128f15723d846fd3fbfb97c3a5c
Remi Melisson <remi.melisson@inria.fr>
2010-04-21 14:52:27 +0000

    update quickfix
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/IDE/trunk@530 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- org.ow2.fractal.mind.xtext.ui/src/org/ow2/fractal/mind/xtext/ui/quickfix/FractalQuickfixProvider.java
+++ org.ow2.fractal.mind.xtext.ui/src/org/ow2/fractal/mind/xtext/ui/quickfix/FractalQuickfixProvider.java
@@ -1,6 +1,7 @@
 package org.ow2.fractal.mind.xtext.ui.quickfix;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.ui.editor.model.edit.IModification;
 import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
 import org.eclipse.xtext.ui.editor.model.edit.ISemanticModification;
@@ -10,15 +11,23 @@ import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
 import org.ow2.fractal.mind.xtext.validation.FractalJavaValidator;
 
+import adl.ArgumentDefinition;
+import adl.FormalArgument;
+import adl.ImportDefinition;
 import adl.InterfaceDefinition;
+import adl.TemplateSpecifier;
 
 public class FractalQuickfixProvider extends DefaultQuickfixProvider {
 
+	/*
+	 * Renaming quickfixes
+	 */
+	
 	@Fix(FractalJavaValidator.DUPLICATE_INTERFACE_NAME)
-	public void capitalizeName(final Issue issue,
+	public void renameInterface(final Issue issue,
 			IssueResolutionAcceptor acceptor) {
-		acceptor.accept(issue, "Rename interface",
-				"Change this name in order to make it unique.", null,
+		acceptor.accept(issue, "Rename",
+				"Rename this interface", null,
 				new ISemanticModification() {
 					public void apply(EObject element,
 							IModificationContext context) throws Exception {
@@ -26,23 +35,86 @@ public class FractalQuickfixProvider extends DefaultQuickfixProvider {
 						if (element instanceof InterfaceDefinition) {
 							InterfaceDefinition interfaceDefinition = (InterfaceDefinition) element;
 							String name = interfaceDefinition.getName();
-							if (Character.isDigit(name
-									.charAt(name.length()-1))) {
-								name = name.substring(0, name.length() - 1);
-								int nbr = (Character.valueOf((name.charAt(name
-										.length()-1)))+1);
-								name= name + nbr;
-							} else
-								name = name + 1;
-							interfaceDefinition.setName(name);
+							
+							interfaceDefinition.setName(rename(name));
 						}
 
 					}
 				});
 	}
+	
+	@Fix(FractalJavaValidator.DUPLICATE_FORMAL_ARGUMENT_NAME)
+	public void renameArgument(final Issue issue, 
+			IssueResolutionAcceptor acceptor) {
+		acceptor.accept(issue, "Rename",
+				"Rename this duplicate argument.", null,
+				new ISemanticModification() {
+
+					@Override
+					public void apply(EObject element,
+							IModificationContext context) throws Exception {
+						
+						if (element instanceof FormalArgument) {
+							FormalArgument formalDefinition = (FormalArgument) element;
+							String newName = rename(formalDefinition.getName());
+							
+							formalDefinition.setName(newName);
+						}
+					}
+				});
+	}
+	
+	@Fix(FractalJavaValidator.DUPLICATE_TEMPLATE_SPECIFIER_NAME)
+	public void renameTemplateSpecifierName(final Issue issue, 
+			IssueResolutionAcceptor acceptor) {
+		acceptor.accept(issue, "Rename",
+				"Rename duplicate template specifier name.", null,
+				new ISemanticModification() {
+
+					@Override
+					public void apply(EObject element,
+							IModificationContext context) throws Exception {
+						
+						if (element instanceof TemplateSpecifier) {
+							TemplateSpecifier templateSpecifier= (TemplateSpecifier) element;
+							String newName = rename(templateSpecifier.getName());
+							
+							templateSpecifier.setName(newName);
+						}
+					}
+				});
+	}
+	
+	/**
+	 * Rename, for example : 
+	 * 		toto -> toto1
+	 * 		toto1 -> toto2
+	 * @param name to refactor
+	 * @return
+	 */
+	public static String rename(String name){
+		
+		String newName;
+		
+		if (Character.isDigit(name
+				.charAt(name.length()-1))) {
+			newName = name.substring(0, name.length() - 1);
+			int nbr = Integer.parseInt(((name.substring(name.length()-1)))) + 1;
+			newName = newName + nbr ;
+			
+		} else {
+			newName = name + 1;
+		}
+		
+		return newName;
+	}
+	
+	/*
+	 * Unknown element quickfixes 
+	 */
 
 	@Fix(FractalJavaValidator.UNKNOWN_IMPORT)
-	public void fixImport(final Issue issue, IssueResolutionAcceptor acceptor) {
+	public void fixUnkownImport(final Issue issue, IssueResolutionAcceptor acceptor) {
 		acceptor.accept(issue, "Remove unknown import",
 				"Remove this unavailable package.", null,
 				new ISemanticModification() {
@@ -50,11 +122,29 @@ public class FractalQuickfixProvider extends DefaultQuickfixProvider {
 					@Override
 					public void apply(EObject element,
 							IModificationContext context) throws Exception {
-						// TODO Auto-generated method stub
+						if (element instanceof ImportDefinition)
+							EcoreUtil.delete(element);
 					}
 				});
 	}
+	
+	@Fix(FractalJavaValidator.UNKNOWN_TEMPLATE_SPECIFIER_TYPE)
+	public void fixUnknownTemplateSpecifierType(final Issue issue, IssueResolutionAcceptor acceptor) {
+		acceptor.accept(issue, "Remove",
+				"Remove unknown template specifier type", null,
+				new ISemanticModification() {
 
+					@Override
+					public void apply(EObject element,
+							IModificationContext context) throws Exception {
+						if (element instanceof TemplateSpecifier)
+							EcoreUtil.delete(element);
+					}
+				});
+	}	
+	
+//TODO complete when ModelToProjectUtil facilities will be available
+/*
 	@Fix(FractalJavaValidator.UNKNOWN_INTERFACE)
 	public void createInterface(final Issue issue,
 			IssueResolutionAcceptor acceptor) {
@@ -67,10 +157,10 @@ public class FractalQuickfixProvider extends DefaultQuickfixProvider {
 					public void apply(IModificationContext context)
 							throws Exception {
 
-						// TODO create interface
 						// ModelToProjectUtil.INSTANCE.
 
 					}
 				});
 	}
+*/
 }
