6bc0581a9a929cacc5569decbc9d6aed6940bc41
Olivier Marot <olivier.marot@logica.com>
2010-06-21 15:42:15 +0000

    - constraint for duplicate sub component name - removed edit policy for CONNECTIONHANDLESROLE (note attachment)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/mindEd/trunk@1053 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- org.ow2.mindEd.adl.editor.graphic.ui/customsrc/org/ow2/mindEd/adl/editor/graphic/ui/custom/constraints/JavaConstraints.java
+++ org.ow2.mindEd.adl.editor.graphic.ui/customsrc/org/ow2/mindEd/adl/editor/graphic/ui/custom/constraints/JavaConstraints.java
@@ -1,5 +1,13 @@
 package org.ow2.mindEd.adl.editor.graphic.ui.custom.constraints;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.validation.IValidationContext;
+import org.ow2.mindEd.adl.ArchitectureDefinition;
+import org.ow2.mindEd.adl.Element;
 import org.ow2.mindEd.adl.SubComponentDefinition;
 import org.ow2.mindEd.adl.ComponentKind;
 
@@ -15,5 +23,24 @@ public class JavaConstraints {
 	
 	public static boolean isUndefinedSubComponent(SubComponentDefinition domainElement) {
 		return (!isPrimitiveSubComponent(domainElement) && ! isCompositeSubComponent(domainElement));
+	}
+
+	public static IStatus subCompSameName(SubComponentDefinition context,
+			IValidationContext ctx) {
+
+		EList<Element> elements = context.getParentBody().getElements();
+		List<SubComponentDefinition> subComponents = new ArrayList<SubComponentDefinition>();
+		for (Element elem : elements) {
+			if (elem instanceof SubComponentDefinition &&
+					elem != context)
+				subComponents.add((SubComponentDefinition) elem);
+		}
+		
+		for (SubComponentDefinition subComp : subComponents) {
+			if (subComp.getName().equals(context.getName()))
+				return ctx.createFailureStatus(context);
+		}
+		
+		return ctx.createSuccessStatus();
 	}
 }
