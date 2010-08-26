b72f813dd61da06e9d134a34b77fbc03106b2858
Luc Bastian <luc.bastian@logica.com>
2010-08-26 12:17:11 +0000

    Simulation Drag&Drop -Drop ADL in Primitive Component and Primitive Sub Component
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/mindEd/branches/DND@1196 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

--- org.ow2.mindEd.adl.editor.graphic.ui/customsrc/org/ow2/mindEd/adl/editor/graphic/ui/custom/edit/commands/tempDND.java
+++ org.ow2.mindEd.adl.editor.graphic.ui/customsrc/org/ow2/mindEd/adl/editor/graphic/ui/custom/edit/commands/tempDND.java
@@ -30,9 +30,11 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.internal.PluginAction;
 import org.eclipse.ui.internal.WorkbenchPlugin;
 import org.ow2.mindEd.adl.AdlPackage;
+import org.ow2.mindEd.adl.ArchitectureDefinition;
 import org.ow2.mindEd.adl.ComponentReference;
 import org.ow2.mindEd.adl.ReferencesList;
 import org.ow2.mindEd.adl.custom.impl.AdlDefinitionCustomImpl;
+import org.ow2.mindEd.adl.custom.impl.CompositeReferenceDefinitionCustomImpl;
 import org.ow2.mindEd.adl.custom.impl.FileCCustomImpl;
 import org.ow2.mindEd.adl.custom.impl.ImplementationDefinitionCustomImpl;
 import org.ow2.mindEd.adl.custom.impl.InterfaceDefinitionCustomImpl;
@@ -41,10 +43,22 @@ import org.ow2.mindEd.adl.custom.impl.PrimitiveReferenceDefinitionCustomImpl;
 import org.ow2.mindEd.adl.custom.impl.SubComponentDefinitionCustomImpl;
 import org.ow2.mindEd.adl.editor.graphic.ui.custom.edit.parts.AdlDefinitionCustomEditPart;
 import org.ow2.mindEd.adl.editor.graphic.ui.custom.edit.parts.CompositeBodyCompartmentCustomEditPart;
+import org.ow2.mindEd.adl.editor.graphic.ui.custom.edit.parts.PrimitiveBodyCompartmentCustomEditPart;
+import org.ow2.mindEd.adl.editor.graphic.ui.custom.edit.parts.PrimitiveComponentDefinitionCustomEditPart;
+import org.ow2.mindEd.adl.editor.graphic.ui.custom.edit.parts.PrimitiveSubComponentCustomEditPart;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.ComponentTypeDefinitionCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.CompositeReferenceCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.CompositeReferencesListCreateCommand;
 import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.InterfaceDefinitionCreateCommand;
 import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.PrimitiveComponentDefinitionCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.PrimitiveReferenceCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.PrimitiveReferencesListCreateCommand;
 import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.PrimitiveSubComponentCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.PrimitiveSubReferenceCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.TypeReferenceCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.TypeReferencesListCreateCommand;
 import org.ow2.mindEd.adl.editor.graphic.ui.edit.commands.UndefinedSubComponentCreateCommand;
+import org.ow2.mindEd.adl.editor.graphic.ui.edit.parts.SubComponentPrimitiveBodyCompartmentEditPart;
 import org.ow2.mindEd.adl.editor.graphic.ui.providers.MindElementTypes;
 import org.ow2.mindEd.adl.impl.CompositeReferenceDefinitionImpl;
 import org.ow2.mindEd.adl.impl.ElementImpl;
@@ -75,35 +89,58 @@ public class tempDND extends AbstractPopupMenu implements IObjectActionDelegate{
 					GraphicalEditPart EP = (GraphicalEditPart)element;
 					IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("test_mind");
 					MindProject mindProject = ModelToProjectUtil.INSTANCE.getMindProject(project);
+					
+					// Get Mind File
 					MindFile mindFile = mindProject.findMindFile("mindpkg.df");
 					if(mindFile != null)
 					{
+						// Test Type of Mind File
 						if(mindFile instanceof MindAdl)
 						{
+							// Type of Mind File is ADL
 							EditElementCommand command = null;
-							
+
 							if(element instanceof CompositeBodyCompartmentCustomEditPart)
 							{
+								// If Drop on Composite Body
+								// Create new Undefined Primitive Sub-Component
 								CreateElementRequest createNewUndefinedSubComponent = new CreateElementRequest(EP.getEditingDomain(), EP.resolveSemanticElement() , MindElementTypes.SubComponentDefinition_3153);
 								command = new UndefinedSubComponentCreateCommand(createNewUndefinedSubComponent);
 							}
 							else if(element instanceof AdlDefinitionCustomEditPart)
 							{
-								CreateElementRequest createNewUndefinedSubComponent = new CreateElementRequest(EP.getEditingDomain(), EP.resolveSemanticElement() , MindElementTypes.PrimitiveComponentDefinition_2008);
-								command = new PrimitiveComponentDefinitionCreateCommand(createNewUndefinedSubComponent); 
+								// If Drop on ADL Definition
+								// Create new Primitive Component
+								CreateElementRequest createNewPrimitiveComponent = new CreateElementRequest(EP.getEditingDomain(), EP.resolveSemanticElement() , MindElementTypes.PrimitiveComponentDefinition_2008);
+								command = new PrimitiveComponentDefinitionCreateCommand(createNewPrimitiveComponent); 
 							}
-/*
-//							CreateElementRequest createNewUndefinedSubComponent = new CreateElementRequest(EP.getEditingDomain(), EP.resolveSemanticElement() , MindElementTypes.SubComponentDefinition_3153);
-//							UndefinedSubComponentCreateCommand createUndefinedSubComponent = new UndefinedSubComponentCreateCommand(createNewUndefinedSubComponent);
+							else if(element instanceof SubComponentPrimitiveBodyCompartmentEditPart)
+							{
+								// If Drop on Primitive Sub Component
+								
+								PrimitiveSubComponentCustomEditPart primitiveSubCompo = (PrimitiveSubComponentCustomEditPart) EP.getParent().getParent();
+								
+								CreateElementRequest createNewExtends = new CreateElementRequest(
+										primitiveSubCompo.getEditingDomain(), 
+										primitiveSubCompo.resolveSemanticElement() , 
+										MindElementTypes.CompositeComponentDefinition_2007);
+				
+								command = new PrimitiveSubReferenceCreateCommand(createNewExtends);
+							}
+							else if(element instanceof PrimitiveBodyCompartmentCustomEditPart)
+							{
+								// If Drop on Primitive Component
+								PrimitiveComponentDefinitionCustomEditPart primitiveCompo = (PrimitiveComponentDefinitionCustomEditPart) EP.getParent().getParent();
+								
+								CreateElementRequest createNewExtends = new CreateElementRequest(
+										primitiveCompo.getEditingDomain(), 
+										primitiveCompo.resolveSemanticElement() , 
+										MindElementTypes.PrimitiveReferencesList_3124);
+				
+								command = new PrimitiveReferencesListCreateCommand(createNewExtends);
+							}
 							
-							CreateElementRequest createNewUndefinedSubComponent = new CreateElementRequest(EP.getEditingDomain(), EP.resolveSemanticElement() , MindElementTypes.PrimitiveComponentDefinition_2008);
-							EditCommandRequestWrapper wrapper = new EditCommandRequestWrapper(createNewUndefinedSubComponent);
-							CreateElementCommand temp = new CreateElementCommand(createNewUndefinedSubComponent);
-							
-							PrimitiveComponentDefinitionCreateCommand createUndefinedSubComponent = new PrimitiveComponentDefinitionCreateCommand(createNewUndefinedSubComponent); 
-							
-//							UndefinedSubComponentCreateCommand createUndefinedSubComponent = new UndefinedSubComponentCreateCommand(createNewUndefinedSubComponent);
-*/
+
 							if(command != null)
 							{
 								if(command.canExecute())
@@ -113,10 +150,9 @@ public class tempDND extends AbstractPopupMenu implements IObjectActionDelegate{
 									TransactionImpl transaction = new TransactionImpl(EP.getEditingDomain(), false);
 									
 									try {
-										
-										
 										if(element instanceof CompositeBodyCompartmentCustomEditPart)
 										{
+											// If Drop on Composite Body
 											transaction.start();
 											CompositeReferenceDefinitionImpl newReferenceDefinition = new CompositeReferenceDefinitionImpl();
 											newReferenceDefinition.setReferenceName(mindFile.getQualifiedName());
@@ -127,6 +163,7 @@ public class tempDND extends AbstractPopupMenu implements IObjectActionDelegate{
 										}
 										else if(element instanceof AdlDefinitionCustomEditPart)
 										{
+											// If Drop on ADL Definition
 											transaction.start();
 											
 											PrimitiveComponentDefinitionCustomImpl primitiveComponentDefinition = (PrimitiveComponentDefinitionCustomImpl) commandResult.getReturnValue();
@@ -137,6 +174,27 @@ public class tempDND extends AbstractPopupMenu implements IObjectActionDelegate{
 											primitiveComponentDefinition.setReferencesList(referenceList);
 											newReferenceDefinition.setParentReferencesList(referenceList);
 
+											transaction.commit();
+										}
+										else if(element instanceof SubComponentPrimitiveBodyCompartmentEditPart)
+										{
+											// If Drop on Primitive Sub-Component
+											transaction.start();
+											
+											CompositeReferenceDefinitionCustomImpl referenceDefinition = (CompositeReferenceDefinitionCustomImpl) commandResult.getReturnValue();
+										
+											referenceDefinition.setReferenceName(mindFile.getQualifiedName());
+											
+											transaction.commit();
+										}
+										else if(element instanceof PrimitiveBodyCompartmentCustomEditPart)
+										{
+											transaction.start();
+											
+											PrimitiveReferencesListImpl referenceList = (PrimitiveReferencesListImpl) commandResult.getReturnValue();
+											
+											referenceList.getReferences().get(0).setReferenceName(mindFile.getQualifiedName());
+											
 											transaction.commit();
 										}
 										
@@ -152,13 +210,21 @@ public class tempDND extends AbstractPopupMenu implements IObjectActionDelegate{
 						else {
 							if(mindFile instanceof MindC)
 							{
-								CreateElementRequest createNewPrimitiveSubComponent = new CreateElementRequest(EP.getEditingDomain(), EP.resolveSemanticElement() , MindElementTypes.SubComponentDefinition_3135);
-								PrimitiveSubComponentCreateCommand createCommandPrimitiveSubComponent = new PrimitiveSubComponentCreateCommand(createNewPrimitiveSubComponent);
-								if(createCommandPrimitiveSubComponent != null){
-									if(createCommandPrimitiveSubComponent.canExecute()){	
-										createCommandPrimitiveSubComponent.execute(null, null);
+								// Type of Mind File is MindC
+								EditElementCommand command = null;
+								
+								if(element instanceof CompositeBodyCompartmentCustomEditPart)
+								{
+									// If Drop on Composite Body
+									CreateElementRequest createNewPrimitiveSubComponent = new CreateElementRequest(EP.getEditingDomain(), EP.resolveSemanticElement() , MindElementTypes.SubComponentDefinition_3135);
+									command = new PrimitiveSubComponentCreateCommand(createNewPrimitiveSubComponent);
+								}
+								
+								if(command != null){
+									if(command.canExecute()){	
+										command.execute(null, null);
 										
-										CommandResult commandResult = createCommandPrimitiveSubComponent.getCommandResult();
+										CommandResult commandResult = command.getCommandResult();
 										SubComponentDefinitionCustomImpl temp = (SubComponentDefinitionCustomImpl)commandResult.getReturnValue();
 										
 										
