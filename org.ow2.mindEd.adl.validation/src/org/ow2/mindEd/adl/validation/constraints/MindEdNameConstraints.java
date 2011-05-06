85cde378267a4974aacf24d09a513c7ad9d3771a
Luc Bastian <luc.bastian@logica.com>
2011-05-06 15:10:23 +0000

    Implement Validation plugin and Constraint
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/mind/mindEd/trunk@1421 a01ce9dd-6950-4f94-9599-cd5ca46ef4f0

---
+++ org.ow2.mindEd.adl.validation/src/org/ow2/mindEd/adl/validation/constraints/MindEdNameConstraints.java
@@ -0,0 +1,55 @@
+package org.ow2.mindEd.adl.validation.constraints;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.ow2.mindEd.adl.custom.impl.InterfaceDefinitionCustomImpl;
+import org.ow2.mindEd.adl.custom.impl.SubComponentDefinitionCustomImpl;
+import org.ow2.mindEd.adl.impl.AnnotationImpl;
+import org.ow2.mindEd.adl.impl.ArchitectureDefinitionImpl;
+import org.ow2.mindEd.adl.impl.ComponentReferenceImpl;
+
+public class MindEdNameConstraints extends AbstractModelConstraint{
+
+	@Override
+	public IStatus validate(IValidationContext ctx) {
+		String name = null;
+		
+		if(ctx.getTarget() instanceof ArchitectureDefinitionImpl)
+		{
+			Object temp = (ArchitectureDefinitionImpl)ctx.getTarget();
+			name = ((ArchitectureDefinitionImpl)ctx.getTarget()).getName();
+		}
+		else if(ctx.getTarget() instanceof InterfaceDefinitionCustomImpl)
+		{
+			name = ((InterfaceDefinitionCustomImpl)ctx.getTarget()).getName();
+		}
+		else if(ctx.getTarget() instanceof SubComponentDefinitionCustomImpl )
+		{
+			Object temp = (SubComponentDefinitionCustomImpl)ctx.getTarget();
+			name = ((SubComponentDefinitionCustomImpl)ctx.getTarget()).getName();
+		}
+		else if(ctx.getTarget() instanceof ComponentReferenceImpl )
+		{
+			name = ((ComponentReferenceImpl)ctx.getTarget()).getNameFQN();
+		}
+		else if(ctx.getTarget() instanceof AnnotationImpl )
+		{
+			Object temp = (AnnotationImpl)ctx.getTarget();
+			name = ((AnnotationImpl)ctx.getTarget()).getName();
+		}
+		else
+		{
+			System.out.println("Validate failure");
+		}
+		
+		if( (name == null) || (name.length() == 0) )
+		{
+			return ctx.createFailureStatus("Name of this item cannot be empty.");
+		}
+		else
+			return ctx.createSuccessStatus();
+
+	}
+
+}
