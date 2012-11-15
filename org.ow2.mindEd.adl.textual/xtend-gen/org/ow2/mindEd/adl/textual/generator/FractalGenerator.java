e58014e48b492d5a063a9259a7e892183fd91fae
Seyvoz Stéphane <sseyvoz@assystem.com>
2012-11-15 17:54:52 +0100

    Regeneration

---
+++ org.ow2.mindEd.adl.textual/xtend-gen/org/ow2/mindEd/adl/textual/generator/FractalGenerator.java
@@ -0,0 +1,11 @@
+package org.ow2.mindEd.adl.textual.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class FractalGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+  }
+}
