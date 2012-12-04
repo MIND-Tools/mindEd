b9f8c2e81109233a9a55a538e3d84c5508114818
Seyvoz Stéphane <sseyvoz@assystem.com>
2012-12-04 10:59:36 +0100

    Adding missing files from last commit, complex attributes support

---
+++ org.ow2.mindEd.adl.textual/src-gen/org/ow2/mindEd/adl/textual/fractal/Value.java
@@ -0,0 +1,19 @@
+/**
+ */
+package org.ow2.mindEd.adl.textual.fractal;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Value</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.ow2.mindEd.adl.textual.fractal.FractalPackage#getValue()
+ * @model
+ * @generated
+ */
+public interface Value extends EObject
+{
+} // Value
---
+++ org.ow2.mindEd.adl.textual/src-gen/org/ow2/mindEd/adl/textual/fractal/impl/ValueListImpl.java
@@ -0,0 +1,165 @@
+/**
+ */
+package org.ow2.mindEd.adl.textual.fractal.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import org.ow2.mindEd.adl.textual.fractal.FractalPackage;
+import org.ow2.mindEd.adl.textual.fractal.Value;
+import org.ow2.mindEd.adl.textual.fractal.ValueList;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Value List</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.ow2.mindEd.adl.textual.fractal.impl.ValueListImpl#getMember <em>Member</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ValueListImpl extends ValueImpl implements ValueList
+{
+  /**
+   * The cached value of the '{@link #getMember() <em>Member</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getMember()
+   * @generated
+   * @ordered
+   */
+  protected EList<Value> member;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected ValueListImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return FractalPackage.Literals.VALUE_LIST;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<Value> getMember()
+  {
+    if (member == null)
+    {
+      member = new EObjectContainmentEList<Value>(Value.class, this, FractalPackage.VALUE_LIST__MEMBER);
+    }
+    return member;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs)
+  {
+    switch (featureID)
+    {
+      case FractalPackage.VALUE_LIST__MEMBER:
+        return ((InternalEList<?>)getMember()).basicRemove(otherEnd, msgs);
+    }
+    return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType)
+  {
+    switch (featureID)
+    {
+      case FractalPackage.VALUE_LIST__MEMBER:
+        return getMember();
+    }
+    return super.eGet(featureID, resolve, coreType);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public void eSet(int featureID, Object newValue)
+  {
+    switch (featureID)
+    {
+      case FractalPackage.VALUE_LIST__MEMBER:
+        getMember().clear();
+        getMember().addAll((Collection<? extends Value>)newValue);
+        return;
+    }
+    super.eSet(featureID, newValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID)
+  {
+    switch (featureID)
+    {
+      case FractalPackage.VALUE_LIST__MEMBER:
+        getMember().clear();
+        return;
+    }
+    super.eUnset(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID)
+  {
+    switch (featureID)
+    {
+      case FractalPackage.VALUE_LIST__MEMBER:
+        return member != null && !member.isEmpty();
+    }
+    return super.eIsSet(featureID);
+  }
+
+} //ValueListImpl
